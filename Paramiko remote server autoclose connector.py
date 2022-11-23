338c96b1102b8e72251eec41820ec595f12c38fa
Lines14 <tonybass1414@yahoo.com>
2022-11-23 23:39:28 +0600

    hide examples of "sensitive" data from templates

--- Paramiko remote server autoclose connector.py
+++ Paramiko remote server autoclose connector.py
@@ -1,6 +1,16 @@
 import warnings
 from cryptography.utils import CryptographyDeprecationWarning
 from contextlib import closing
+import os
+
+# Подтягивает переменные окружения из .bashrc:;
+# get() в случае отсутствия входящих данных выводит None вместо ошибки:
+
+SSH_HOST = os.environ.get('SSH_HOST')
+SSH_USERNAME = os.environ.get('SSH_USERNAME')
+SSH_PKEY = os.environ.get('SSH_PKEY')
+SSH_PORT = os.environ.get('SSH_PORT')
+SSH_PORT = int(SSH_PORT)
 
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category=CryptographyDeprecationWarning)
@@ -8,7 +18,7 @@ with warnings.catch_warnings():
 
     with closing(paramiko.SSHClient()) as client:
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect('192.168.1.215', 2222, username='lines14', key_filename='/home/lines14/.ssh/id_ed25519')
+        client.connect(SSH_HOST, SSH_PORT, username=SSH_USERNAME, key_filename=SSH_PKEY)
 
         stdin, stdout, stderr = client.exec_command('ls -a')
         for y in stdout.readlines():
--- Paramiko remote server connector.py
+++ Paramiko remote server connector.py
@@ -1,12 +1,23 @@
 import warnings
 from cryptography.utils import CryptographyDeprecationWarning
+import os
+
+# Подтягивает переменные окружения из .bashrc;
+# get() в случае отсутствия входящих данных выводит None вместо ошибки:
+
+SSH_HOST = os.environ.get('SSH_HOST')
+SSH_USERNAME = os.environ.get('SSH_USERNAME')
+SSH_PKEY = os.environ.get('SSH_PKEY')
+SSH_PORT = os.environ.get('SSH_PORT')
+SSH_PORT = int(SSH_PORT)
+
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category=CryptographyDeprecationWarning)
     import paramiko
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('192.168.1.215', 2222, username='lines14', key_filename='/home/lines14/.ssh/id_ed25519')
+client.connect(SSH_HOST, SSH_PORT, username=SSH_USERNAME, key_filename=SSH_PKEY)
 
 stdin, stdout, stderr = client.exec_command('ifconfig')
 for y in stdout.readlines():
